a052623f73efeb763f6a3cdafb8549c852a02559
Amaury Bauzac <amaury.bauzac@gmail.com>
2017-04-12 13:40:52 +0200

    All, todo and done buttons

--- grafikart-todo/src/components/Todos.vue
+++ grafikart-todo/src/components/Todos.vue
@@ -1,27 +1,36 @@
 <template type="html">
-
-<section  class="todoapp">
-    <header class="header">
-        <h1>Todos</h1>
-        <input type="text" class="new-todo" placeholder="Ajouter" v-model="newTodo" @keyup.enter="addTodo"
-        >
-    </header>
-    <div class="main">
-        <ul class="todo-list">
-            <li class="todo" v-for="todo in todos" :class="{completed : todo.completed}">
-                <div class="view">
-                    <input type="checkbox" class="toggle__input" v-model="todo.completed"  :id="todo.labelid">
-                    <label class="toggle" :for="todo.labelid"></label>
-                    <div class="label">{{ todo.name }}</div>
-                </div>
-            </li>
-        </ul>
-    </div>
-    <footer class="footer">
-        <span class="todo-count"><strong>{{ remaining }}</strong> taches a faire</span>
-    </footer>
-</section>
-
+    <section  class="todoapp">
+        <header class="header">
+            <h1>Todos</h1>
+            <input type="text" class="new-todo" placeholder="Ajouter" v-model="newTodo" @keyup.enter="addTodo"
+            >
+        </header>
+        <div class="main">
+            <ul class="todo-list">
+                <li class="todo" v-for="todo in filteredTodos" :class="{completed : todo.completed}">
+                    <div class="view">
+                        <input type="checkbox" class="toggle__input" v-model="todo.completed"  :id="todo.labelid">
+                        <label class="toggle" :for="todo.labelid"></label>
+                        <div class="label">{{ todo.name }}</div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <footer class="footer">
+            <span class="todo-count"><strong>{{ remaining }}</strong> taches a faire</span>
+            <ul class="filters">
+                <li>
+                    <a href="#" :class="{selected : filter === 'all'}" @click.prevent="filter = 'all'">Toutes </a>
+                </li>
+                <li>
+                    <a href="#" :class="{selected : filter === 'todo'}" @click.prevent="filter = 'todo'">A faire </a>
+                </li>
+                <li>
+                    <a href="#" :class="{selected : filter === 'done'}" @click.prevent="filter = 'done'">Faites </a>
+                </li>
+            </ul>
+        </footer>
+    </section>
 </template>
 
 <script>
@@ -32,7 +41,7 @@ function newID(){
 }
 
 export default {
- // 13 min 14sec
+ // 18 min 14sec
   data () {
     return {
       todos: [{
@@ -40,7 +49,8 @@ export default {
                 completed: false,
                 labelid : newID()
             }],
-            newTodo: ''
+            newTodo: '',
+            filter: 'all'
         }
     },
     methods: {
@@ -57,6 +67,15 @@ export default {
     computed: {
         remaining(){
             return this.todos.filter(todo => !todo.completed).length
+        },
+        filteredTodos (){
+            if(this.filter == 'todo'){
+                return this.todos.filter(todo => !todo.completed)
+            }
+            else if(this.filter == 'done'){
+                this.todos.filter(todo => todo.completed)
+            }
+            return this.todos;
         }
     }
 }
